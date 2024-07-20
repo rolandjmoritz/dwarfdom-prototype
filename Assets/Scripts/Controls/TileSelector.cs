580752741f7717328e25ab5f7e9df279ad8b704e
RolandJMoritz <rolandjmoritz@gmail.com>
2024-07-20 20:21:27 +0200

    can select tiles

---
+++ Assets/Scripts/Controls/TileSelector.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.AI.Navigation;
+using UnityEngine;
+
+public class TileSelector : MonoBehaviour
+{
+    public NavMeshSurface surface;
+
+    private void Start()
+    {
+        if (surface == null)
+        {
+            surface = FindObjectOfType<NavMeshSurface>();
+        }
+    }
+
+    public void UpdateNavMesh()
+    {
+        surface.BuildNavMesh();
+    }
+
+    void Update()
+    {
+        // Check for mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            DetectMeshClick();
+        }
+    }
+
+    void DetectMeshClick()
+    {
+        // Create a ray from the camera to the mouse position
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // Perform the raycast
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Check if the object hit by the raycast has a collider
+            Collider collider = hit.collider;
+
+            if (collider != null)
+            {
+                // Handle the selection logic
+                HandleSelection(collider.gameObject);
+            }
+        }
+    }
+
+    void HandleSelection(GameObject selectedObject)
+    {
+        // Here you can handle what happens when a mesh is selected
+        Debug.Log("Selected object: " + selectedObject.name);
+
+        WallTile tile = selectedObject.GetComponent<WallTile>();
+        if (tile != null)
+        {
+            tile.Selected = !tile.Selected;
+        }
+    }
+}
--- Assets/Scripts/Level/WallTile.cs
+++ Assets/Scripts/Level/WallTile.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class WallTile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public MeshRenderer meshRenderer;
+    public Material normalMaterial;
+    public Material selectedMaterial;
 
-    // Update is called once per frame
-    void Update()
+    public enum tileType
     {
-        
+        rock, 
+        wall, 
+        gold
+    };
+
+    private bool selected = false;
+
+    public bool Selected {
+        get
+        {
+            return selected;
+        } 
+        set
+        {
+            selected = value;
+            if (selected)
+            {
+                meshRenderer.material = selectedMaterial;
+            }
+            else
+            {
+                meshRenderer.material = normalMaterial;
+            }
+        }
     }
 }
